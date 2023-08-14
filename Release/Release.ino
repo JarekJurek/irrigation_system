70382ff1f2aba8ae5f65e3b752fe2c7173870d8d
JarekJurek <grzes.pawlak@gmail.com>
2023-08-14 23:13:36 +0200

    fix: syntax and major bugs

--- Release/Release.ino
+++ Release/Release.ino
@@ -29,7 +29,7 @@
 
 struct ts t;  // for RTC
 
-const int rainSensorPin = 23;	// ESP32's pin number for rain sensor
+const int rainSensorPin = 23;   // ESP32's pin number for rain sensor
 bool rainDetected = 0;
 
 const int valve1 = 27;  // defining ESP32's valve port
@@ -39,15 +39,15 @@ const int valve4 = 33;
 const int valve5 = 25;  
 const int valve6 = 26;
 
-int offCount = 0;		// variable for the main turn-off switch
-bool offButton = 0;		// the state of the turn-off switch
+int offCount = 0;   // variable for the main turn-off switch
+bool offButtonOn = false;   // the state of the turn-off switch
 int lastOffHour = 0;
 
-int startH1 = 0;		// start hour for vale 1
-int startM1 = 0;		// start minute
-int runTime1 = 20;		// run time in minutes
-int count1 = 0;			// couter for minutes with open valve
-int lastMin1 = 0;		// for catching that minute has changed
+int startH1 = 0;    // start hour for vale 1
+int startM1 = 0;    // start minute
+int runTime1 = 20;  // run time in minutes
+int count1 = 0;     // couter for minutes with open valve
+int lastMin1 = 0;   // for catching that minute has changed
 bool valveButton1 = 0;  // inital state of valve's button
 int virtualPin1 = 1;
 
@@ -91,11 +91,11 @@ int lastMin6 = 0;
 bool valveButton6 = 0;
 int virtualPin6 = 6;
 
-BLYNK_WRITE(V0) {				// Main turn-off button
-  int valueHigh = param.asInt();	// assigning incoming value from pin Vx to a variable, x is virtual pin number
-  if (valueHigh) {			// If the button is ON, the offCount starts, 
-    offButtonOn = true;			// storing the state of button 
-    offCount = 1;				// the offButton's clock starts
+BLYNK_WRITE(V0) {                 // Main turn-off button
+  int valueHigh = param.asInt();  // assigning incoming value from pin Vx to a variable, x is virtual pin number
+  if (valueHigh) {                // If the button is ON, the offCount starts, 
+    offButtonOn = true;           // storing the state of button 
+    offCount = 1;                 // the offButton's clock starts
     lastOffHour = t.hour;
   } else {
     offButtonOn = false;
@@ -108,10 +108,10 @@ BLYNK_WRITE(V1) {
   digitalWrite(valve1, !valueHigh);  // align button state with valve state (HIGHT state at valve pin == valve closed)
   if (valueHigh) {
     valveButton1 = 1;
-	lastMin1 = t.min;
+    lastMin1 = t.min;
   } else {
     valveButton1 = 0;
-	count1 = 0;
+    count1 = 0;
   }
 }
 
@@ -120,10 +120,10 @@ BLYNK_WRITE(V2) {
   digitalWrite(valve2, !valueHigh);
   if (valueHigh) {
     valveButton2 = 1;
-	lastMin2 = t.min;
+    lastMin2 = t.min;
   } else {
     valveButton2 = 0;
-	count2 = 0;
+    count2 = 0;
   }
 }
 
@@ -132,10 +132,10 @@ BLYNK_WRITE(V3) {
   digitalWrite(valve3, !valueHigh);
   if (valueHigh) {
     valveButton3 = 1;
-	lastMin3 = t.min;
+    lastMin3 = t.min;
   } else {
     valveButton3 = 0;
-	count3 = 0;
+    count3 = 0;
   }
 }
 
@@ -144,10 +144,10 @@ BLYNK_WRITE(V4) {
   digitalWrite(valve4, !valueHigh);
   if (valueHigh) {
     valveButton4 = 1;
-	lastMin4 = t.min;
+    lastMin4 = t.min;
   } else {
     valveButton4 = 0;
-	count4 = 0;
+    count4 = 0;
   }
 }
 
@@ -156,10 +156,10 @@ BLYNK_WRITE(V5) {
   digitalWrite(valve5, !valueHigh);
   if (valueHigh) {
     valveButton5 = 1;
-	lastMin5 = t.min;
+    lastMin5 = t.min;
   } else {
     valveButton5 = 0;
-	count5 = 0;
+    count5 = 0;
   }
 }
 
@@ -168,37 +168,38 @@ BLYNK_WRITE(V6) {
   digitalWrite(valve6, !valueHigh);
   if (valueHigh) {
     valveButton6 = 1;
-	lastMin6 = t.min;
+    lastMin6 = t.min;
   } else {
     valveButton6 = 0;
-	count6 = 0;
+    count6 = 0;
   }
 }
 
 void changeValveState(int valveNr, int virtualPin, bool &valveButtonOn, bool isOpen) {
-	digitalWrite(valveNr, !isOpen);			// changing valve state, 0 is open
-    Blynk.virtualWrite(virtualPin, isOpen);	// changing valve's button to ON (schedule run)
+  digitalWrite(valveNr, !isOpen);             // changing valve state, 0 is open
+    Blynk.virtualWrite(virtualPin, isOpen);   // changing valve's button to ON (schedule run)
     valveButtonOn = isOpen;
 }
 
-void startOnCondition(int valveNr, int runTime, int &count, int &lastMin, int virtualPin, bool &valveButtonOn, int startH, int startM) {
-  if ((startH == t.hour) && (startM == t.min) && (!offButtonOn)) { // start run condition
-    lastMin = t.min;			// saving the actual minute
-	changeValveState(valveNr, virtualPin, valveButtonOn, true);  // opening the valve, 0 is open
+void startOnCondition(int valveNr, int &lastMin, int virtualPin, bool &valveButtonOn, int startH, int startM) {
+  if ((startH == t.hour) && (startM == t.min) && (!offButtonOn)) {  // start run condition
+    lastMin = t.min;                                                // saving the actual minute
+  changeValveState(valveNr, virtualPin, valveButtonOn, true);       // opening the valve, 0 is open
   }
 }
 
 void stopOnCondition(int valveNr, int runTime, int &count, int &lastMin, int virtualPin, bool &valveButtonOn) {
   if (!valveButtonOn) {  // is not running
-	return;
+    return;
+  }
 
   if ((count < runTime) && (lastMin != t.min)) { // continue run condition and minute later
     count += 1;
     lastMin = t.min;
   }
 
-  if ((count >= runTime) || (offButtonOn)) {	// close valve condition
-  	changeValveState(valveNr, virtualPin, valveButtonOn, false); // close valve
+  if ((count >= runTime) || (offButtonOn)) {                      // close valve condition
+    changeValveState(valveNr, virtualPin, valveButtonOn, false);  // close valve
     count = 0;
   }
 }
@@ -241,15 +242,15 @@ void loop() {
   DS3231_get(&t);
   BlynkEdgent.run();
 
-  rainDetected = !digitalRead(rainSensor);  // sensor's 1 - no rain, 0 - rain
+  rainDetected = !digitalRead(rainSensorPin);  // sensor's 1 - no rain, 0 - rain
 
-  if (rainDetected && !offButtonOn) {  // start run-disabled condition
-    Blynk.virtualWrite(0, 1);	// putting on the off button ON
-    offButtonOn = true;			// the button bool variable == 1
+  if (rainDetected && !offButtonOn) {   // start run-disabled condition
+    Blynk.virtualWrite(0, 1);           // putting on the off button ON
+    offButtonOn = true;                 // the button bool variable == 1
     lastOffHour = t.hour;
   } else if (offButtonOn && (offCount < 24) && (lastOffHour != t.hour)) {  // continue run-disabled condition
     lastOffHour = t.hour;
-	offCount += 1;
+    offCount += 1;
   } else if (offCount == 24) {  // stop  run-disabled condition
     Blynk.virtualWrite(0, 0);
     offButtonOn = false;
@@ -257,22 +258,21 @@ void loop() {
   }
 
   // main functions
-  runOnCondition(valve1, runTime1, count1, lastMin1, virtualPin1, valveButton1, startH1, startM1);
+  startOnCondition(valve1, lastMin1, virtualPin1, valveButton1, startH1, startM1);
   stopOnCondition(valve1, runTime1, count1, lastMin1, virtualPin1, valveButton1);
 
-  runOnCondition(valve2, runTime2, count2, lastMin2, virtualPin2, valveButton2, startH2, startM2);
+  startOnCondition(valve2, lastMin2, virtualPin2, valveButton2, startH2, startM2);
   stopOnCondition(valve2, runTime2, count2, lastMin2, virtualPin2, valveButton2);
 
-  runOnCondition(valve3, runTime3, count3, lastMin3, virtualPin3, valveButton3, startH3, startM3);
+  startOnCondition(valve3, lastMin3, virtualPin3, valveButton3, startH3, startM3);
   stopOnCondition(valve3, runTime3, count3, lastMin3, virtualPin3, valveButton3);
 
-  runOnCondition(valve4, runTime4, count4, lastMin4, virtualPin4, valveButton4, startH4, startM4);
+  startOnCondition(valve4, lastMin4, virtualPin4, valveButton4, startH4, startM4);
   stopOnCondition(valve4, runTime4, count4, lastMin4, virtualPin4, valveButton4);
 
-  runOnCondition(valve5, runTime5, count5, lastMin5, virtualPin5, valveButton5, startH5, startM5);
+  startOnCondition(valve5, lastMin5, virtualPin5, valveButton5, startH5, startM5);
   stopOnCondition(valve5, runTime5, count5, lastMin5, virtualPin5, valveButton5);
 
-  runOnCondition(valve6, runTime6, count6, lastMin6, virtualPin6, valveButton6, startH6, startM6);
+  startOnCondition(valve6, lastMin6, virtualPin6, valveButton6, startH6, startM6);
   stopOnCondition(valve6, runTime6, count6, lastMin6, virtualPin6, valveButton6);
-
 }
