5e9a39057b315dc4fbd166c282be9f9561613c2a
JarekJurek <grzes.pawlak@gmail.com>
2023-10-14 16:14:21 +0200

    ref: print using timer

--- utils/rtc_time/rtc_time.ino
+++ utils/rtc_time/rtc_time.ino
@@ -1,39 +1,43 @@
+#include "Blynk.h"
 #include <Wire.h>
 #include "ds3231.h"
 
 struct ts t;  // Time structure for the RTC
 
+BlynkTimer timer;
+
+void updateBlynkTime() {
+  DS3231_get(&t);
+  publishTime(t.hour, t.min, t.sec);
+}
+
+void publishTime(int hour, int minute, int sec) {
+  Serial.print("Time: ");
+  Serial.print(hour);
+  Serial.print(":");
+  Serial.print(minute);
+  Serial.print(":");
+  Serial.println(sec);
+}
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
 
-  // // Uncomment the lines below to set the time initially
-  // t.hour = 14;  // Set the initial time here
-  // t.min = 15;
+  // Uncomment the lines below to set the time initially
+  // t.hour = 15;  // Set the initial time here
+  // t.min = 58;
   // t.sec = 0;
   // t.mday = 14;  // Day of the month
   // t.mon = 10;    // Month
   // t.year = 2023; // Year
   // t.wday = 6;   // Day of the week: 0 = Sunday, 1 = Monday, etc.
-  
-  // // Set the time
-  // DS3231_set(t);
+  // DS3231_set(t); // set time
 
-  // Initialize the RTC
   DS3231_init(DS3231_CONTROL_INTCN);
+  timer.setInterval(1000L, updateBlynkTime);
 }
 
 void loop() {
-  // Get the current time
-  DS3231_get(&t);
-
-  // Print the current time to the Serial Monitor
-  Serial.print("Time: ");
-  Serial.print(t.hour);
-  Serial.print(":");
-  Serial.print(t.min);
-  Serial.print(":");
-  Serial.println(t.sec);
-
-  delay(1000); // Wait for a second
+  timer.run();
 }
